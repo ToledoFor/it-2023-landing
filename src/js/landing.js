3a699cc66ebf98d9e3ed407b62a9d63a2f0cfaf4
Dmitriy Olhovsky <dmitriy.olhovsky@beetroot.se>
2023-11-21 10:55:36 +0200

    21.11 lesson

--- src/js/landing.js
+++ src/js/landing.js
@@ -1,1 +1,64 @@
-console.log('Hello from landing page!')
+console.log('Hello from "landing" page!')
+
+const var1 = 'test'
+const obj1 = {
+    k1: 1,
+    k2: 2,
+}
+
+let str1 = 't'
+str1 = "str \"test\""
+// str1 = `test ${obj1.k1}`
+
+console.log(str1)
+
+var cond = 5
+
+// if (cond !== '5') {
+//     console.log('true')
+// } else {
+//     console.log('false')
+// }
+
+for (let i = 0, x = 5 ; ; i++,x = i+2) {
+    console.log(`loop ${i}, ${x}`);
+    
+    if (i == 5) {
+        break;
+    }
+}
+
+console.log(cond !== '5' ? 'true' : 'false')
+
+// {
+// const x = 10
+// }
+// console.log(x)
+
+
+//console.log(5 - '3')
+
+// const obj2 = obj1
+
+// console.log(obj1)
+// console.log(obj2)
+
+// obj1.k1 = 'test'
+// obj2.k2 = 'test1'
+
+// console.log(obj1)
+// console.log(obj2)
+
+// let var2 = 123
+
+// var var3 = 5
+
+const f1 = function () {
+    var x = 5
+    console.log('hello from func')
+}
+
+const f2 = f1
+
+f1()
+f2()
